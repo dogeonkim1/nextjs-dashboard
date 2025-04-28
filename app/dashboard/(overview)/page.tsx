4801584f27aaab491a77580504a1b327a7ae45fc
dogeonkim1 <caddibo4@naver.com>
2025-04-28 15:46:40 +0900

    feat: add Suspense

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,15 +1,13 @@
-import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import {revenue} from "@/app/lib/placeholder-data";
-import { fetchLatestInvoices, fetchCardData} from '@/app/lib/data';
+import { fetchCardData} from '@/app/lib/data';
 import { Suspense } from 'react';
-import {LatestInvoicesSkeleton, RevenueChartSkeleton} from '@/app/ui/skeletons';
+import {CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton} from '@/app/ui/skeletons';
 
 export default async function Page() {
-
-    const latestInvoices = await fetchLatestInvoices();
 
     const {
         numberOfCustomers,
@@ -23,6 +21,9 @@ export default async function Page() {
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <Suspense fallback={<CardsSkeleton />}>
+                    <CardWrapper />
+                </Suspense>
                 <Card title="Collected" value={totalPaidInvoices} type="collected" />
                 <Card title="Pending" value={totalPendingInvoices} type="pending" />
                 <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
@@ -37,7 +38,7 @@ export default async function Page() {
                     <RevenueChart />
                 </Suspense>
                 <Suspense fallback={<LatestInvoicesSkeleton />}>
-                    <RevenueChart />
+                    <LatestInvoices />
                 </Suspense>
             </div>
         </main>
