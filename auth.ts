a5ad32dcaa9d369bb7b893320f0631d22cc7aacb
dogeonkim1 <caddibo4@naver.com>
2025-04-29 15:47:07 +0900

    feat: 사용자 쿼리 전송 함수

--- auth.ts
+++ auth.ts
@@ -2,6 +2,20 @@ import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import {z} from "zod";
+import {User} from "@/app/lib/definitions";
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
+
+async function getUser(email:string): Promise<User | undefined>{
+    try{
+        const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+        return user[0];
+    } catch (error){
+        console.error('Failed to fetch user:', error);
+        throw new Error('Failed to fetch user');
+    }
+}
 
 export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
@@ -10,6 +24,13 @@ export const { auth, signIn, signOut } = NextAuth({
             const parsedCredentials = z
                 .object({email:z.string().email(),password: z.string().min(6)})
                 .safeParse(credentials);
+
+            if(parsedCredentials.success){
+                const {email, password} = parsedCredentials.data;
+                const user = await getUser(email);
+                if (!user) return null;
+            }
+            return null;
             },
         }),
     ],
