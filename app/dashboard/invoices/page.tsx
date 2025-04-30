dc44c7cf3b21bb7a1a855f63f7d7b538aa97dd1e
dogeonkim1 <caddibo4@naver.com>
2025-04-30 17:25:02 +0900

    feat: 인보이스클라이언프 페이지 연동

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,19 +1,11 @@
-import Pagination from '@/app/ui/invoices/pagination';
-import Search from '@/app/ui/search';
-import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
-import { lusitana } from '@/app/ui/fonts';
-import { Suspense } from 'react';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { fetchInvoicesPages } from '@/app/lib/data';
 import { Metadata } from 'next';
+import InvoicesClientPage from './InvoicesClientPage';
 
 export const metadata: Metadata = {
     title: 'Invoices',
 };
 
-// Next.js에서 사용하는 searchParams는 기본적으로 URLSearchParams에서 파싱된 값입니다.
-// 이를 기반으로 searchParams의 타입을 명시합니다.
 interface PageProps {
     searchParams?: {
         query?: string;
@@ -22,26 +14,16 @@ interface PageProps {
 }
 
 export default async function Page({ searchParams }: PageProps) {
-    const query = searchParams?.query || '';
-    const currentPage = Number(searchParams?.page) || 1;
+    const query = searchParams?.query ?? '';
+    const currentPage = Number(searchParams?.page ?? '1');
 
     const totalPages = await fetchInvoicesPages(query);
 
     return (
-        <div className="w-full">
-            <div className="flex w-full items-center justify-between">
-                <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
-            </div>
-            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-                <Search placeholder="Search invoices..." />
-                <CreateInvoice />
-            </div>
-            <Suspense key={`${query}-${currentPage}`} fallback={<InvoicesTableSkeleton />}>
-                <Table query={query} currentPage={currentPage} />
-            </Suspense>
-            <div className="mt-5 flex w-full justify-center">
-                <Pagination totalPages={totalPages} />
-            </div>
-        </div>
+        <InvoicesClientPage
+            query={query}
+            currentPage={currentPage}
+            totalPages={totalPages}
+        />
     );
 }
